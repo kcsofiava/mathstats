4ab9a5f0b3e1d63bb8ebca446e90044ee965a37a
kcsofiava <kcsofiava@gmail.com>
2020-06-29 14:26:08 -0400

    python3 script for Hackerrank Day 7: Pearson Corr Coeff challenge

---
+++ hackerrank_pearson_corr_coeff.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 29 14:23:52 2020
+
+@author: kcsof
+"""
+
+#Given two n-element data sets, X and Y , calculate the value of the Pearson correlation coefficient.
+
+N = int(input())
+X = list(map(float,input().strip().split()))
+Y = list(map(float,input().strip().split()))
+
+mu_x = sum(X) / N
+mu_y = sum(Y) / N
+
+stdv_x = (sum([(i - mu_x)**2 for i in X]) / N)**0.5
+stdv_y = (sum([(i - mu_y)**2 for i in Y]) / N)**0.5
+
+
+covariance = sum([(X[i] - mu_x) * (Y[i] -mu_y) for i in range(N)])
+
+correlation_coefficient = covariance / (N * stdv_x * stdv_y)
+
+print(round(correlation_coefficient,3))
